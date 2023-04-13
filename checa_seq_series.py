37476a7877d4e5643f3429bb4e955656e667de98
cesardddp <cesardddp@hotmail.com>
2023-04-13 10:31:43 -0300

    add checagem de consulta e sequencia no makefile

--- checa_seq_series.py
+++ checa_seq_series.py
@@ -1,10 +1,44 @@
-seq =()
-for i, n in enumerate(seq):
+seq1 = []
+seq2 = []
+
+import subprocess
+
+def sendmessage(message):
+    subprocess.Popen(['notify-send', message])
+    return
+
+with open('nNFE_atual.log', 'r') as f:
+    #exemplo line serie1: SÉRIE 1: 3943, nfe original:35221146364058000115550010000017911035692790 []
+    #exemplo line serie2: SÉRIE 2: 42601 nfe original:35221046364058000115550020000060871685858158 []
+
+
+    for line in f.readlines():
+        if line.startswith('SÉRIE 1: '):
+            seq1.append(int(line.split()[2]))
+        elif line.startswith('SÉRIE 2: '):
+            seq2.append(int(line.split()[2]))
+
+
+for i, n in enumerate(seq1):
     try:
-        if n+1 != seq[i+1]:
+        if n+1 != seq1[i+1]:
             print(n)
     except IndexError:
-        if(seq[-1]==n):
+        if(seq1[-1]==n):
             print("OK")
+            sendmessage("Sequencia 1 OK")
         else:
             print('falhou em: '+str(n))
+            sendmessage("Sequencia falhou em "+str(n))
+
+for i, n in enumerate(seq2):
+    try:
+        if n+1 != seq2[i+1]:
+            print(n)
+    except IndexError:
+        if(seq2[-1]==n):
+            print("OK")
+            sendmessage("Sequencia 2 OK")
+        else:
+            print('falhou em: '+str(n))
+            sendmessage("Sequencia falhou em "+str(n))
--- makefile
+++ makefile
@@ -1,14 +1,17 @@
 PATH_ZIP = log
-FILENAME = $(shell date +%Y_%m_%d_%H_%M_%S)log.zip
+FILENAME = $(shell date +%Y-%m-%d_%H:%M:%S)log.zip
 TARGET_FOLDER= /home/$(USER)/Documentos
 generate:
 	python ./nfs_complementares.py NFS/Originais NFS/Complementares NFS/Base
 
 generate_and_send:
-	python  ./nfs_complementares.py --envio-producao NFS/Originais NFS/Complementares NFS/Base 3124 42601 
+	python  ./nfs_complementares.py --envio-producao NFS/Originais NFS/Complementares NFS/Base 
 
 generate_and_homo:
-	python  ./nfs_complementares.py --envio-homologacao NFS/Originais NFS/Complementares NFS/Base 3124 42601 
+	python  ./nfs_complementares.py --envio-homologacao NFS/Originais NFS/Complementares NFS/Base 
+	python  ./nfs_complementares.py --envio-homologacao\
+	 home/dev/Downloads/enviando_nfes_complementares/novembro/MES\ 11\ SERIE\ 1\
+	  NFS/Complementares NFS/Base 
 
 limpeza:
 	rm -rf ./NFS/Complementares/*
@@ -28,4 +31,11 @@ limpeza-geral:
 	rm -rf ./consultas/*
 	rm -rf ./log/*
 	
-	
+testa_consultas:
+	cp testa_consultadas.py consultas;\
+	cd consultas;\
+	python testa_consultadas.py;\
+	code falharam
+
+teste_sequencia:
+	python checa_seq_series.py
